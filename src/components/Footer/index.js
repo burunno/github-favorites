c9cab7ca855d0e504e38161a2ae16c3e85b00d8b
Bruno <bruno.carv@icloud.com>
2019-05-10 11:43:10 -0300

    duck pattern

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import propTypes from "prop-types";
+import { connect } from "react-redux";
+
+const Footer = ({ count }) => <p>Você tem {count} favotiros</p>;
+
+Footer.propTypes = {
+  count: propTypes.number.isRequired
+};
+
+const mapStateToProps = state => ({
+  count: state.favorites.data.length
+});
+
+export default connect(mapStateToProps)(Footer);
---
+++ src/pages/main/index.js
@@ -0,0 +1,79 @@
+import React, { Fragment, Component } from "react";
+import propTypes from "prop-types";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Creators as FavoriteActions } from "../../store/ducks/favorites";
+
+class Main extends Component {
+  static propTypes = {
+    addFavoriteRequest: propTypes.func.isRequired,
+    favorites: propTypes.shape({
+      loadig: propTypes.bool,
+      data: propTypes.arrayOf(
+        propTypes.shape({
+          id: propTypes.number,
+          name: propTypes.string,
+          description: propTypes.string,
+          url: propTypes.string
+        })
+      ),
+      error: propTypes.oneOfType([null, propTypes.string])
+    }).isRequired
+  };
+
+  state = {
+    repositoryInput: ""
+  };
+
+  handleAddRepository = event => {
+    event.preventDefault();
+
+    this.props.addFavoriteRequest(this.state.repositoryInput);
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+
+          {this.props.favorites.loading && <span>Carregando...</span>}
+
+          {!!this.props.favorites.error && (
+            <span style={{ color: "#F00" }}>{this.props.favorites.error}</span>
+          )}
+        </form>
+
+        <ul>
+          {this.props.favorites.data.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>{favorite.name} : </strong>
+                {favorite.description}
+              </p>
+              <a href={favorite.url}>Acessar Repositório</a>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  favorites: state.favorites
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Main);
